26979de0671bc2003d87b52e74288d041bef10ec
gu-log <113324088+gu-log@users.noreply.github.com>
2023-02-11 04:12:09 +0900

    Update folder.page.ts

--- frontend/src/app/folder/folder.page.ts
+++ frontend/src/app/folder/folder.page.ts
@@ -12,7 +12,7 @@ export class FolderPage implements OnInit {
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id')
   }
 
 }
